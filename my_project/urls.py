0bf2daeae071fae4c531982dfa5a2aff9a2fa74e
Sofie Andersson <sofie@koikidsstudio.com>
2024-02-12 08:24:13 +0000

    Update hello_world to home

--- my_project/urls.py
+++ my_project/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from hello_world import views as index_views
+from home import views as index_views
 from recipes import views as recipes_views
 from about import views as about_views
 from contact import views as contact_views
 
 urlpatterns = [
-    path('hello/', index_views.index, name='index'),
+    path('home/', index_views.index, name='index'),
     path('recipes/', recipes_views.recipes_lively, name='recipes'),
     path('about/', about_views.about_lively, name='about'),
     path('contact/', contact_views.contact_lively, name='contact'),
