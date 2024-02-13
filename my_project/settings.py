bd8b7795d067478f54ab434cf4570aa6539ecd60
Sofie Andersson <sofie@koikidsstudio.com>
2024-02-13 20:53:18 +0000

    Add blog app

--- my_project/settings.py
+++ my_project/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-idamariasofie-lively-h5haxtiiq74.ws-eu108.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-idamariasofie-lively-c1udhnsu0wn.ws-eu108.gitpod.io','.herokuapp.com']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'home',
     'recipes',
     'about',
+    'blog',
     'contact',
 ]
 
--- my_project/urls.py
+++ my_project/urls.py
@@ -3,12 +3,14 @@ from django.urls import path, include
 from home import views as index_views
 from recipes import views as recipes_views
 from about import views as about_views
+from blog import views as blog_views
 from contact import views as contact_views
 
 urlpatterns = [
     path('home/', index_views.index, name='index'),
     path('recipes/', recipes_views.recipes_lively, name='recipes'),
     path('about/', about_views.about_lively, name='about'),
+    path('blog/', blog_views.blog_lively, name='blog'),
     path('contact/', contact_views.contact_lively, name='contact'),
     path('admin/', admin.site.urls),
 ]
