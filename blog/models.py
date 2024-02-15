82b92c8e8199d1e2614da70576c2100deea1dd59
Sofie Andersson <sofie@koikidsstudio.com>
2024-02-15 10:21:02 +0000

    Create Comment model

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Recipe(models.Model):
+    """ A model for creating a recipe post """
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts")
     title = models.CharField(max_length=300, unique=True)
@@ -18,3 +19,15 @@ class Recipe(models.Model):
         User, related_name='blogpost_like', blank=True)
     featured = models.BooleanField()
     status = models.IntegerField(choices=STATUS, default=0)
+
+
+class Comment(models.Model):
+    """ A model to allow and manage comments on recipe blog posts """
+    user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comments", null=True)
+    recipe_id = models.CharField(max_length=500)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+
