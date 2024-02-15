a39c1a6a470a025bcea9b46c2f21d785d73e0517
Sofie Andersson <sofie@koikidsstudio.com>
2024-02-15 09:27:59 +0000

    Update name to class Recipe

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
-class Post(models.Model):
+class Recipe(models.Model):
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts")
     title = models.CharField(max_length=300, unique=True)
