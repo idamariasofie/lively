423cda5d494fac2a577ac8a43cebde4cbe1026a0
Sofie Andersson <sofie@koikidsstudio.com>
2024-02-17 12:29:15 +0000

    Add filter PostList

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,5 @@ from .models import Recipe
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Recipe.objects.all()
+    queryset = Recipe.objects.all().order_by("-created_on")
     template_name = "post_list.html"
