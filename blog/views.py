3e6399b2caecd8f89790547ddc3f634728f27462
Sofie Andersson <sofie@koikidsstudio.com>
2024-02-17 14:27:50 +0000

    Create base.html template

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,5 @@ from .models import Recipe
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Recipe.objects.all().order_by("-created_on")
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
