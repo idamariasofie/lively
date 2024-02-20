2dd561222f25ea0ff058da556f51f9059a0cea07
Sofie Andersson <sofie@koikidsstudio.com>
2024-02-20 09:53:17 +0000

    Updated About url

--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import About
 
 # Create your views here.
 def about_lively(request):
--- my_project/urls.py
+++ my_project/urls.py
@@ -6,12 +6,12 @@ from about import views as about_views
 from contact import views as contact_views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('home/', index_views.index, name='index'),
     path('recipes/', recipes_views.recipes_lively, name='recipes'),
-    path("about/", include("about.urls"), name="about-urls"),
+    path('about/', about_views.about_lively, name='about'),
     path("", include("blog.urls"), name="blog-urls"),
     path('contact/', contact_views.contact_lively, name='contact'),
     path("accounts/", include("allauth.urls")),
-    path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
 ]
