eb1ab1fec0be2b32d22921d32c588379520860ba
Sofie Andersson <sofie@koikidsstudio.com>
2024-02-20 18:54:00 +0000

    Create comment form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Recipe
+from .forms import CommentForm
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -24,9 +25,17 @@ def recipe_detail(request, slug):
 
     queryset = Recipe.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comment.filter(approved=True).count()
+    comment_form = CommentForm()
 
     return render(
         request,
         "blog/recipe_detail.html",
-        {"post": post},
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+        },
     )
