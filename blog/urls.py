b5988f800d50617b500ca8b68668caac9c3ff7cb
Sofie Andersson <sofie@koikidsstudio.com>
2024-02-21 20:32:15 +0000

    Update database

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name='home'),
     path('recipes/<slug:slug>/', views.recipe_detail, name='recipe_detail'),
-    #path('recipes/', views.recipe_list, name='recipes'),
     path('about/', views.about, name='about'),
     path('blog/', views.blog_page, name='blog_page'),
     path('contact/', views.contact, name='contact'),
--- my_project/urls.py
+++ my_project/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from home import views as index_views
-from blog import urls as blog_urls
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', include("blog.urls"), name='about'),
+    path('', include('blog.urls')),  # Include the 'blog' app's URLs for the root path
+    path('about/', blog_views.about, name='about'),
     path('home/', index_views.index, name='index'),
-    path('recipes/', include("blog.urls"), name='recipes'),
-    path('blog/', include("blog.urls"), name="blog-urls"),
-    path('contact/', include("blog.urls"), name='contact'),
+    path('recipes/', blog_views.recipe_list, name='recipes'),
+    path('blog/', blog_views.blog_page, name="blog-urls"),
+    path('contact/', blog_views.contact, name='contact'),
     path("accounts/", include("allauth.urls")),
     path('summernote/', include('django_summernote.urls')),
 ]
