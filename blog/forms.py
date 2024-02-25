6f75e5e52e9e79689ed125a997644b91c27c1204
Sofie Andersson <sofie@koikidsstudio.com>
2024-02-25 11:23:56 +0000

    Add RecipeForm

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,13 @@
-from .models import Comment
+from .models import Comment, Recipe
 from django import forms
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('body',)
+        fields = ('name', 'email', 'body', 'created_on')
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ('title', 'slug', 'created_on', 'comment_count', 'content', 'time_to_prepare', 'overview', 'likes', 'featured')
