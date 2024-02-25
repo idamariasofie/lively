d1d0ffae98cc61523171854b1de90be675567c2a
Sofie Andersson <sofie@koikidsstudio.com>
2024-02-25 11:28:41 +0000

    Removed created_on in forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -5,9 +5,9 @@ from django import forms
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('name', 'email', 'body', 'created_on')
+        fields = ('name', 'email', 'body')
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ('title', 'slug', 'created_on', 'comment_count', 'content', 'time_to_prepare', 'overview', 'likes', 'featured')
+        fields = ('title', 'slug', 'comment_count', 'content', 'time_to_prepare', 'overview', 'likes', 'featured')
