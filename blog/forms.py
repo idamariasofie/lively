9f22722aff29b5bb23a70fb62cb377bc1e015a18
Sofie Andersson <sofie@koikidsstudio.com>
2024-03-02 22:52:16 +0000

    Add recipes to test updates

--- blog/forms.py
+++ blog/forms.py
@@ -12,11 +12,26 @@ class SearchForm(forms.Form):
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('body',) 
+        fields = ('body',)
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, user=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['body'].widget.attrs['placeholder'] = 'Add your comment...'
+
+        if user:
+            # If user is provided, set the initial value and make it a hidden field
+            self.fields['user'] = forms.ModelChoiceField(
+                queryset=user,
+                widget=forms.HiddenInput(),
+                initial=user,
+            )
+        else:
+            # If user is not provided, keep it as a regular field
+            self.fields['user'] = forms.ModelChoiceField(queryset=user)
+
+    class Meta:
+        model = Comment
+        fields = ('body', 'user')
 
 class RecipeForm(forms.ModelForm):
     class Meta:
